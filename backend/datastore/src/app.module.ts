5af8b1bb312d9b75d2653e2dde10afe843fa0779
Collin Bolles <collinbolles@gmail.com>
2023-03-12 22:04:14 -0400

    GraphQL setup

--- backend/datastore/src/app.module.ts
+++ backend/datastore/src/app.module.ts
@@ -1,10 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), './schema.gql'),
+    }),
+    AuthModule
+  ],
 })
 export class AppModule {}
