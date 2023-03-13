106292b46b27b8f244af3e07bbf87f8b4a5feefd
Collin Bolles <collinbolles@gmail.com>
2023-03-13 17:57:49 -0400

    feat: Prescription on User (#3)
    * Add prescription to user
    * Remove prescription from user

--- backend/datastore/src/auth/jwt.strategy.ts
+++ backend/datastore/src/auth/jwt.strategy.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User, UserDocument } from './user.model';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(configService: ConfigService, @InjectModel(User.name) private readonly userModel: Model<UserDocument>) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +16,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: any) {
-    return payload;
+  async validate(payload: { _id: string }): Promise<User> {
+    const user = await this.userModel.findById(payload._id).exec();
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
   }
 }
--- backend/datastore/src/prescription/prescription.service.ts
+++ backend/datastore/src/prescription/prescription.service.ts
@@ -3,13 +3,38 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { NewPrescription } from './dtos/new-prescription.dto';
 import { Prescription, PrescriptionDocument } from './prescription.model';
+import { User, UserDocument } from '../auth/user.model';
 
 @Injectable()
 export class PrescriptionService {
-  constructor(@InjectModel(Prescription.name) private readonly prescriptionModel: Model<PrescriptionDocument>) {}
+  constructor(
+    @InjectModel(Prescription.name) private readonly prescriptionModel: Model<PrescriptionDocument>,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>
+  ) {}
 
   async findAll(): Promise<Prescription[]> {
     return this.prescriptionModel.find().exec();
+  }
+
+  async find(id: string): Promise<Prescription | null> {
+    return this.prescriptionModel.findById(id).exec();
+  }
+
+  async addPrescriptionToUser(user: User, prescription: Prescription): Promise<void> {
+    // If the prescription is already in the user's list, don't add it again
+    if (user.prescriptions.includes(prescription._id)) {
+      return;
+    }
+
+    await this.userModel.findByIdAndUpdate(user._id, { $push: { prescriptions: prescription._id } });
+  }
+
+  async getPrescriptionForUser(user: User): Promise<Prescription[]> {
+    return await this.prescriptionModel.find({ _id: { $in: user.prescriptions } }).exec();
+  }
+
+  async removePrescriptionFromUser(user: User, prescription: Prescription): Promise<void> {
+    await this.userModel.findByIdAndUpdate(user._id, { $pull: { prescriptions: prescription._id } });
   }
 
   async createPrescription(prescription: NewPrescription): Promise<Prescription> {
