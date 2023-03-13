c034f153ce2cb1abf73cde72502aa9f2a13a9083
Collin Bolles <collinbolles@gmail.com>
2023-03-12 22:16:27 -0400

    Placeholder auth

--- backend/datastore/src/auth/auth.resolver.ts
+++ backend/datastore/src/auth/auth.resolver.ts
@@ -1,9 +1,14 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import {UnauthorizedException} from '@nestjs/common';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { UserLogin } from './dtos/user-login.dto';
 
 @Resolver()
 export class AuthResolver {
-  @Query(() => String)
-  async hello(): Promise<string> {
-    return 'world';
+  @Mutation(() => String)
+  async login(@Args('credentials') credentials: UserLogin): Promise<string> {
+    if (credentials.username == 'bob' && credentials.password == 'bob') {
+      return 'token';
+    }
+    throw new UnauthorizedException('User login failed');
   }
 }
