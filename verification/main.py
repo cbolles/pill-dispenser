2e18584eb884ce354cf0d797511477f279831cf8
tstrogers <93225913+tstrogers@users.noreply.github.com>
2023-05-02 20:13:40 -0400

    Added pill verification to main.py

--- verification/main.py
+++ verification/main.py
@@ -2,6 +2,14 @@ from dotenv import load_dotenv
 from database import Perscription, PillClient
 import os
 from dispenser import Dispenser
+import cv2
+from tensorflow.lite.python.interpreter import Interpreter
+import numpy as np
+import sys
+import random
+import importlib.util
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 PILL_MAPPING = {
@@ -11,10 +19,75 @@ PILL_MAPPING = {
     'metformin': 4
 }
 
+def detect_pill():
+  #Take picture of pill
+  camera = cv2.VideoCapture(0)
+  return_value, image = camera.read()
+  image_path = 'D:/OpenCV/Images'
+  cv2.imwrite(os.path.join(path,'pill.png'),image)
+
+  #Load label map of pill names
+  label_map_path = 'D:/home/labelmap.txt'
+  with open(label_map_path, 'r') as f:
+      labels = [line.strip() for line in f.readlines()]
+  
+  # Load the Tensorflow Lite model into memory
+  modelpath = 'D:/home/detect.tflite'
+  interpreter = Interpreter(model_path=modelpath)
+  interpreter.allocate_tensors()
+
+  #Get model details
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+  height = input_details[0]['shape'][1]
+  width = input_details[0]['shape'][2]
+
+  float_input = (input_details[0]['dtype'] == np.float32)
+
+  input_mean = 127.5
+  input_std = 127.5
+
+  # Load image and resize to expected shape [1xHxWx3]
+  image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  imH, imW, _ = image.shape 
+  image_resized = cv2.resize(image_rgb, (width, height))
+  input_data = np.expand_dims(image_resized, axis=0)
+
+  # Perform the actual detection by running the model with the image as input
+  interpreter.set_tensor(input_details[0]['index'],input_data)
+  interpreter.invoke()
+
+  # Retrieve detection results
+  boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
+  classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
+  scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
+
+
+  # Loop over all detections and draw detection box if confidence is above minimum threshold
+  for i in range(len(scores)):
+    if ((scores[i] > min_conf) and (scores[i] <= 1.0)):
+
+      # Get bounding box coordinates and draw box
+      # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+      ymin = int(max(1,(boxes[i][0] * imH)))
+      xmin = int(max(1,(boxes[i][1] * imW)))
+      ymax = int(min(imH,(boxes[i][2] * imH)))
+      xmax = int(min(imW,(boxes[i][3] * imW)))
+              
+      cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+      #Identify object name
+      object_name = labels[int(classes[i])]
+
+    return object_name
 
 def verify_pill(pill: Perscription):
-    pass
+  object_name = detect_pill()
 
+  if pill == object_name:
+    return 1
+  else:
+    return 0
 
 def main():
     # Load information from environment file
@@ -40,12 +113,15 @@ def main():
         dispenser.select_pill(PILL_MAPPING[pill.name])
 
         # Ensure the correct pill is selected
-        verify_pill(pill)
+        if verify_pill(pill) == 1:
+          # Dispense the pill
+          dispenser.dispense()
+        else:
+          print("Incorrect pill identified!")
+          print("Pill will not be dispensed!")
+          continue
 
-        # Dispense the pill
-        dispenser.dispense()
-
-
+        
 
 if __name__ == '__main__':
     main()
